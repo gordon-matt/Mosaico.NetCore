d30a91cc8985b84eb61a2d20a276d65eac749e44
vnmatt <gordon_matt@hotmail.com>
2017-04-20 09:26:13 +0930

    Initial Checkin (Still buggy)

---
+++ src/Mosaico.NetCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mosaico.NetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("editor")]
+        public IActionResult Editor()
+        {
+            return View();
+        }
+
+        [Route("error")]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Mosaico.NetCore/Controllers/TemplateController.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mosaico.NetCore.Controllers
+{
+    [Route("templates")]
+    public class TemplateController : Controller
+    {
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public TemplateController(IHostingEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        // NOTE: Ignore {name1}. It's actually just the same as {name}, but MVC routing won't allow us to use {name} twice
+        [Route("{name}/template-{name1}")]
+        public IActionResult Index(string name)
+        {
+            string filePath = Path.Combine(hostingEnvironment.WebRootPath, string.Format("templates/{0}/template-{0}.html", name));
+            string content = System.IO.File.ReadAllText(filePath);
+            return Content(content);
+        }
+    }
+}
